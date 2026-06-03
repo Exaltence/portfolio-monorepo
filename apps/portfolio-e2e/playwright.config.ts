575be3928ff1ae842c681bbd04d898eab6baa54b
Shaun Vercauteren <shaun_vercauteren@hotmail.com>
2026-06-03 18:53:33 +0200

    fix(e2e): fix playwright - nx app startup port collision

--- apps/portfolio-e2e/playwright.config.ts
+++ apps/portfolio-e2e/playwright.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
-import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
@@ -23,13 +22,6 @@ export default defineConfig({
     trace: 'on-first-retry',
     /* Record video for every test. See https://playwright.dev/docs/videos */
     video: 'retain-on-failure',
-  },
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npx nx run portfolio:serve',
-    url: 'http://localhost:4200',
-    reuseExistingServer: true,
-    cwd: workspaceRoot,
   },
   projects: [
     {
